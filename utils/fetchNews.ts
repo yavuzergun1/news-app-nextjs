1ed5f1024a5dcde7c58f3b448313ae978a245a2e
Yavuz Ergun <posta.yavuzergun@gmail.com>
2023-02-28 20:43:31 +0300

    new api key written

--- utils/fetchNews.ts
+++ utils/fetchNews.ts
@@ -1,7 +1,7 @@
 export const fetchSearchNews = async (searchTerm: string) => {
   console.log("FETCHNEWS TERM", searchTerm);
   const res = await fetch(
-    `https://newsapi.org/v2/everything?q=${searchTerm}&apiKey=973aa7f1d7f6474c91c1a37d5aadd1d4`,
+    `https://newsapi.org/v2/everything?q=${searchTerm}&apiKey=68de52d6ae5c4128827da8297eddca00`,
     { cache: "no-cache" }
   );
   const news: News = await res.json();
@@ -12,7 +12,7 @@ export const fetchSearchNews = async (searchTerm: string) => {
 
 export const fetchCategoryNews = async (category: string) => {
   const res = await fetch(
-    `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=973aa7f1d7f6474c91c1a37d5aadd1d4`,
+    `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=68de52d6ae5c4128827da8297eddca00`,
     { next: { revalidate: 40 } }
   );
   const news: News = await res.json();
@@ -21,7 +21,7 @@ export const fetchCategoryNews = async (category: string) => {
 
 export const fetchHomePageNews = async () => {
   const res = await fetch(
-    `https://newsapi.org/v2/top-headlines?country=us&apiKey=$973aa7f1d7f6474c91c1a37d5aadd1d4`,
+    `https://newsapi.org/v2/top-headlines?country=us&apiKey=68de52d6ae5c4128827da8297eddca00`,
     { next: { revalidate: 20 } }
   );
   const news: News = await res.json();
